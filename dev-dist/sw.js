6c948a0451095131a046df36f7285645127ad6c0
skadnan40605 <166492715+Adnanshaikh40605@users.noreply.github.com>
2026-05-17 15:27:58 +0530

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mealseltef"
+    "revision": "0.br61eunj1mg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
